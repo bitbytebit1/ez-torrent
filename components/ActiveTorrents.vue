d89a1b52c595b058b34599ad028a01cc764cb342
Tristan Bemert <tristan@flygresor.se>
2023-08-20 05:24:43 +0200

    feat(ActiveTorrent): go to transmission webui on click

--- components/ActiveTorrents.vue
+++ components/ActiveTorrents.vue
@@ -1,29 +1,30 @@
 <script setup>
 const { data, refresh } = await useFetch('/api/gettorrents')
+
+const { webui } = useRuntimeConfig().public.transmission
+
 function formatBytes(bytes, decimals = 1) {
   if (!+bytes)
     return '0 Bytes'
 
   const k = 1024
   const dm = decimals < 0 ? 0 : decimals
-  const sizes = ['Bytes', 'Kbs', 'Mbs', 'Gbs', 'Tbs', 'Pbs', 'Ebs', 'Zbs', 'Ybs']
+  const sizes = ['Bytes', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y']
 
   const i = Math.floor(Math.log(bytes) / Math.log(k))
 
   return `${Number.parseFloat((bytes / k ** i).toFixed(dm))} ${sizes[i]}`
 }
-setInterval(() => {
-  refresh()
-}, 1000)
 
-function onClick() {
-  const path = useRuntimeConfig().public.transmission.webui
-  window.location.href = path
-}
+onMounted(() => {
+  const interval = setInterval(() => {
+    refresh()
+  }, 1000)
+})
 </script>
 
 <template>
-  <div @click="onClick">
+  <NuxtLink :to="webui">
     <span text-green>{{ formatBytes(data?.downloadSpeed) }}</span> / <span text-red>{{ formatBytes(data?.uploadSpeed) }}</span> / <span text-yellow>{{ data?.activeTorrentCount }}</span>
-  </div>
+  </NuxtLink>
 </template>
